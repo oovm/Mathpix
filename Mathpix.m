d1944455f5c047468b775ab48160a04b9f8f305c
GalAster <galaster@foxmail.com>
2018-12-01 12:34:40 +0800

    pack to a paclet

--- Mathpix.m
+++ Mathpix.m
@@ -1,11 +1,27 @@
+(* ::Package:: *)
+
+(* ::Section:: *)
+(*Declare*)
+
+
+BeginPackage["Mathpix"];
+$MathpixToken::usage = "";
+Mathpix::usage = "";
+Begin["`Private`"];
+
+
+(* ::Section:: *)
+(*API*)
+
+
 $MathpixToken = {"ID", "Key"} -> {
 	"trial",
 	"34f1a4cea0eaca8540c95908b4dc84ab"
 } // AssociationThread;
 MathpixHTTP[img_] := Block[
-	{jpeg, url, header, body},
+	{jpeg, api, header, body},
 	jpeg = "data:image/jpg;base64," <> ExportString[img, {"Base64", "JPEG"}];
-	url = "https://api.mathpix.com/v3/latex";
+	api = "https://api.mathpix.com/v3/latex";
 	header = {
 		"app_id" -> $MathpixToken["ID"],
 		"app_key" -> $MathpixToken["Key"],
@@ -14,14 +30,30 @@ MathpixHTTP[img_] := Block[
 	body = ExportString[{
 		"src" -> jpeg, "formats" -> {"latex" -> "simplified", "mathml" -> True, "wolfram" -> True
 		}}, "json"];
-	HTTPRequest[url, <|"Headers" -> header, "Body" -> body, Method -> "POST"|>]
+	HTTPRequest[api, <|"Headers" -> header, "Body" -> body, Method -> "POST"|>]
 ];
+
+
+(* ::Section:: *)
+(*Methods*)
+
+
+MathpixPOST[http_HTTPRequest] := URLExecute[http, Interactive -> False, "RawJSON"];
+MathpixNormal[raw_] := raw["latex"];
+MathpixDisplay[raw_] := DisplayForm@ImportString@raw["mathml"];
+MathpixExpression[raw_] := InputForm@WolframAlpha[raw["wolfram"], "WolframParse"];
+MathpixConfidence[raw_] := "TODO";
+
+
+(* ::Section:: *)
+(*Interface*)
+
+
 Mathpix[path_String, o___] := Mathpix[Import@path, o];
 Mathpix[img_Image, method_ : N] := MathpixInterface[MathpixPOST@MathpixHTTP@img];
 Mathpix[obj_Association, method_ : N] := MathpixInterface[First@obj];
 MathpixInterface[raw_Association, m_] := Block[
-	{},
-	(*Todo: Sow Error*)
+	{},(*Todo: Sow Error*)
 	Switch[m,
 		N, MathpixNormal@raw,
 		E, MathpixExpression@raw,
@@ -30,8 +62,15 @@ MathpixInterface[raw_Association, m_] := Block[
 		_, Iconize[raw, "Mathpix"]
 	]
 ];
-MathpixPOST[http_HTTPRequest] := URLExecute[http, Interactive -> False, "RawJSON"];
-MathpixNormal[raw_] := raw["latex"];
-MathpixDisplay[raw_] := DisplayForm@ImportString@raw["mathml"];
-MathpixExpression[raw_] := InputForm@WolframAlpha[raw["wolfram"], "WolframParse"];
-MathpixConfidence[raw_] := "TODO";
+
+
+(* ::Section:: *)
+(*Additional*)
+
+
+End[];
+SetAttributes[
+	{ },
+	{Protected, ReadProtected}
+];
+EndPackage[]
---
+++ PacletInfo.m
@@ -0,0 +1,7 @@
+(* Paclet Info File *)
+
+Paclet[
+	Name -> "Mathpix",
+	Version -> "1.0.0",
+	MathematicaVersion -> "11.1+"
+]
